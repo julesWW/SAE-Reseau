c6bc28954ff27a56c477ca8ec44f0c4abad23cff
Lohan Marchand <lohan.marchand@etu.unistra.fr>
2025-05-16 15:01:36 +0200

    ajout de m23

---
+++ m23/build/main.c.d
@@ -0,0 +1,3 @@
+build/./main.c.o: main.c graphe.h algos.h
+graphe.h:
+algos.h:
---
+++ m23/utils.c
@@ -0,0 +1,9 @@
+#include "utils.h"
+
+void generer_permutation(size_t *permutation, size_t n)
+{
+}
+
+void init_aleatoire(double *val, size_t n, double max)
+{
+}
