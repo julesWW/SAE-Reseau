ba8da858d38111b98a4bd7ac9e595134d9f6ab7f
Jules Wolff Walk <experimentwindows@gmail.com>
2025-06-08 16:54:08 +0200

    Ajout et lecture du fichier de config

--- Projet/reseau.c
+++ Projet/reseau.c
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "reseau.h"
 #include "m23/algos.h"
 #include <stdio.h>
@@ -82,4 +84,104 @@ void afficher_switch(Switch *sw) {
         afficher_mac(&sw->table_commutation[i].mac);
         printf(" -> Port %u\n", sw->table_commutation[i].port);
     }
+}
+
+//Début du main
+int main() {
+    
+    Reseau_Local reseau;
+    size_t nb_equipements;
+    size_t nb_liaisons;
+    size_t nb_stations = 0;
+    size_t nb_switchs = 0;
+    
+    //Lecture du fichier de configuration
+    FILE *fconfig = fopen("config.txt", "r");
+    if (fconfig == NULL) {
+        perror("Erreur d'ouverture du fichier de configuration.\n");
+        return EXIT_FAILURE;
+    }
+
+    char ligne[256];
+    size_t nligne = 0;
+    while(fgets(ligne, sizeof(ligne), fconfig)) {
+        nligne++;
+        if(nligne == 1){
+            //Je pars du principe que le fichier de configuration est au bon format mais on pourra mettre une vérification au cas où
+            sscanf(ligne, "%zu %zu", &nb_equipements, &nb_liaisons);
+            printf("Nombre d'équipements : %zu, Nombre de liaisons : %zu\n", nb_equipements, nb_liaisons);
+        }
+        else if(nligne <= nb_equipements){
+            if(ligne[0] == '1'){
+                //C'est une station
+                nb_stations++;
+                char nom_station[50];
+                sprintf(nom_station, "station%zu", nb_stations);
+                Station nom_station;
+                //Récupération de l'adresse MAC
+                unsigned char mac_octets[6];
+                sscanf(ligne + 2, "%u:%u:%u:%u:%u:%u", &mac_octets[0], &mac_octets[1], &mac_octets[2], &mac_octets[3], &mac_octets[4], &mac_octets[5]);
+                MACAddress mac_station;
+                init_MacAddress(&mac_station, mac_octets);
+                //Récupération de l'adresse IP
+                unsigned char ip_octets[4];
+                sscanf(ligne + 20, "%u.%u.%u.%u", &ip_octets[0], &ip_octets[1], &ip_octets[2], &ip_octets[3]);
+                IPAddrV4 ip_station;
+                init_IPAddrV4(&ip_station, ip_octets);
+                init_station(&nom_station, &mac_station, &ip_station); 
+            }
+            if(ligne[0] == '2'){
+                //C'est un switch
+                nb_switchs++;
+                char nom_switch[50];
+                sprintf(nom_switch, "switch%zu", nb_switchs);
+                Switch nom_switch;
+                //Récupération de l'adresse MAC
+                unsigned char mac_octets[6];
+                sscanf(ligne + 2, "%u:%u:%u:%u:%u:%u", &mac_octets[0], &mac_octets[1], &mac_octets[2], &mac_octets[3], &mac_octets[4], &mac_octets[5]);
+                MACAddress mac_switch;
+                init_MacAddress(&mac_switch, mac_octets);
+                //Récupération du nombre de ports
+                size_t nb_ports;
+                sscanf(ligne + 20, "%zu", &nb_ports);
+                //Récupération de la priorité
+                size_t priorite;
+                sscanf(ligne + 22, "%zu", &priorite);
+                //Initialisation du switch
+                init_switch(&nom_switch, &mac_switch, nb_ports, priorite, nb_ports);
+                //Initialisation de la table de commutation
+                /*nom_switch.table_commutation = malloc(nb_ports * sizeof(TableComm));
+                if (nom_switch.table_commutation == NULL) {
+                    fprintf(stderr, "Erreur malloc pour table de commutation.\n");
+                    fclose(fconfig);
+                    return EXIT_FAILURE;
+                }*/
+                ///!\jsp comment faire pour ça car il prend nom_switch comme char et non comme switch/!\
+
+            }
+
+        }
+        else if(nligne <= nb_equipements + nb_liaisons){
+            //Pas encore fait
+        }
+
+    }
+    fclose(fconfig);
+    //Initialisation du réseau
+    reseau.nb_stations = nb_stations;
+    reseau.station = malloc(nb_stations * sizeof(Station));
+    if (reseau.station == NULL) {
+        fprintf(stderr, "Erreur malloc pour les stations.\n");
+        return EXIT_FAILURE;
+    }
+    reseau.nb_switchs = nb_switchs;
+    reseau.switchs = malloc(nb_switchs * sizeof(Switch));
+    if (reseau.switchs == NULL) {
+        fprintf(stderr, "Erreur malloc pour les switches.\n");
+        free(reseau.station);
+        return EXIT_FAILURE;
+    }
+
+
+    return 0;
 }
