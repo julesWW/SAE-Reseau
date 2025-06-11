ae7096a5147dd606986449a8fdccc9411cb77803
Jules Wolff Walk <experimentwindows@gmail.com>
2025-06-12 01:55:33 +0200

    Lecture complète du fichier de configuration et chargement du réseau

--- Projet/reseau.h
+++ Projet/reseau.h
@@ -47,8 +47,8 @@ typedef struct Switch{
 
 //Valeurs pour différencier station et switch
 typedef enum {
-    STATION,
-    SWITCH
+    STATION = 1, //Station
+    SWITCH = 2   //Switch
 } Type_equipement;
 
 //union pouvant contenir un switch ou une station
@@ -61,6 +61,7 @@ typedef union {
 typedef struct{
 	Type_equipement type;
 	Appareil valeur;
+    size_t numero_equipement;
 }Equipement;
 
 //liaison : contient 2 equipement et représente qu'ils sont liés
@@ -93,15 +94,20 @@ typedef struct TrameEthernet
 } TrameEthernet;
 
 //Fonctions d'initialisation
-void init_IPAddrV4(IPAddrV4 *ip, unsigned char octets[4]);
+void init_IPAddrV4(IPAddrV4 *ip);
+//void init_IPAddrV4(IPAddrV4 *ip, unsigned char octets[4]);
 
 void init_MacAddress(MACAddress *mac);
 //void init_MACAddress(MACAddress *mac, unsigned char octets[6]);
 
 
-void init_station(Station *station, MACAddress *mac, IPAddrV4 *ip);
-void init_table_comm(TableComm *table, MACAddress *mac, unsigned int port);
-void init_switch(Switch *sw, MACAddress *mac, size_t nb_ports, size_t priorite, size_t nb_entrees);
+void ajouter_Station(Reseau_Local *reseau, MACAddress *mac, IPAddrV4 *ip);
+void ajouter_Switch(Reseau_Local *reseau, MACAddress *mac, size_t nb_ports, size_t priorite);
+void ajouter_Liaison(Reseau_Local *reseau, Equipement *e1, Equipement *e2, size_t poids);
+
+void init_station(Station *station);
+void init_table_comm(TableComm *table);
+void init_switch(Switch *sw);
 void init_TrameEthernet(TrameEthernet *trame, char preambule[7], char sfd, MACAddress *dest, MACAddress *src, char type[7], char *donnees);
 void init_reseau_local(Reseau_Local *reseau, size_t nb_stations, Station *stations, size_t nb_switches, Switch *switches);
 
