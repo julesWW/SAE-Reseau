79593fefe3d4e0b5664796b2888dfe0accb50483
Lohan Marchand <lohan.marchand@etu.unistra.fr>
2025-06-12 13:14:56 +0200

    honetement jsp mec

--- Projet/graphe.h
+++ Projet/graphe.h
@@ -1,3 +1,12 @@
 #pragma once
 
 #include "reseau.h"
+
+#define UNKNOWN_INDEX ((size_t)-1)
+
+size_t equipement_adjacents(Reseau_Local *reseau, Equipement e, size_t ea[]);
+size_t index_liaison(Reseau_Local *reseau, Liaison l);
+bool existe_liaison(Reseau_Local *reseau, Liaison l);
+size_t index_Equipement(Reseau_Local *reseau, Equipement e);
+size_t nb_liaisons(Reseau_Local *reseau);
+size_t ordre(Reseau_Local *reseau);
