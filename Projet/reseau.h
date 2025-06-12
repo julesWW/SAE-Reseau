6a12ba839667505d890160c711e4d1719394417e
Jules Wolff--Walk <jules.wolff-walk@etu.unistra.fr>
2025-06-12 22:09:42 +0200

    Corrections, ajout de deinit, ajout de vérifications malloc

--- Projet/reseau.h
+++ Projet/reseau.h
@@ -95,22 +95,24 @@ typedef struct TrameEthernet
 
 //Fonctions d'initialisation
 void init_IPAddrV4(IPAddrV4 *ip);
+void deinit_IPAddrV4(IPAddrV4 *ip);
 //void init_IPAddrV4(IPAddrV4 *ip, unsigned char octets[4]);
 
 void init_MacAddress(MACAddress *mac);
+void deinit_MacAddress(MACAddress *mac);
 //void init_MACAddress(MACAddress *mac, unsigned char octets[6]);
 
 
 void ajouter_Station(Reseau_Local *reseau, MACAddress *mac, IPAddrV4 *ip);
 void ajouter_Switch(Reseau_Local *reseau, MACAddress *mac, size_t nb_ports, size_t priorite);
-bool ajouter_Liaison(Reseau_Local *reseau, size_t *e1, size_t *e2, size_t poids);
+bool ajouter_Liaison(Reseau_Local *reseau, size_t e1, size_t e2, size_t poids);
 
 void init_station(Station *station);
 void init_table_comm(TableComm *table);
 void init_switch(Switch *sw);
 void init_TrameEthernet(TrameEthernet *trame, char preambule[7], char sfd, MACAddress *dest, MACAddress *src, char type[7], char *donnees);
-void init_reseau_local(Reseau_Local *reseau, size_t nb_stations, Station *stations, size_t nb_switches, Switch *switches);
-*/
+void init_reseau_local(Reseau_Local *reseau);
+
 
 //Fonctions d'affichage
 void afficher_mac(MACAddress *mac);
